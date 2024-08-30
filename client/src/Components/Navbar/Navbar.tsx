20d23626b1d824b277fc486b483f17f3551ca8a5
Albert Dong <albertddong@hotmail.com>
2024-08-29 23:30:19 -0500

    Installed Tailwind (doesn't work apparently), home page loading

--- client/src/Components/Navbar/Navbar.tsx
+++ client/src/Components/Navbar/Navbar.tsx
@@ -1,24 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../Context/useAuth';
+import './Navbar.css'
 
 const Navbar = () => {
 
     const { isLoggedIn, user, logout } = useAuth()
   
     return (
-        <nav className="relative container mx-auto p-6">
-          <div className="flex items-center justify-between">
-            <div className="flex items-center space-x-20">
-              {/* <Link to="/">
-                <img src={logo} alt="" />
-              </Link> */}
-              <div className="hidden font-bold lg:flex">
-                <Link to="/search" className="text-black hover:text-darkBlue">
-                  Search
-                </Link>
-              </div>
-            </div>
+        <nav className="relative container mx-auto p-6 navContainer">
+          <div className="flex items-center justify-between itemsContainer">
+            <Link to="/">
+              <h3>Home</h3>
+            </Link>
             {isLoggedIn() ? (
               <div className="hidden lg:flex items-center space-x-6 text-back">
                 <div className="hover:text-darkBlue">Welcome, {user?.userType} {user?.userName}</div>
@@ -30,7 +24,7 @@ const Navbar = () => {
                 </a>
               </div>
             ) : (
-              <div className="hidden lg:flex items-center space-x-6 text-back">
+              <div className="hidden lg:flex items-center space-x-6 text-back loggingContainer">
                 <Link to="/login" className="hover:text-darkBlue">
                   Login
                 </Link>
--- client/src/Pages/HomePage/HomePage.tsx
+++ client/src/Pages/HomePage/HomePage.tsx
@@ -10,14 +10,17 @@ const HomePage = () => {
 
   const [books, setBooks] = useState<Book[]>([])
   const [searchParams, setSearchParams] = useSearchParams()
+  const [loading, isLoading] = useState(true)
 
   const { user } = useAuth()
 
   const navigate = useNavigate()
 
   useEffect(() => {
+    isLoading(true)
     axios.get(`${api}book?${searchParams.toString()}`).then((response) => {
       setBooks(response.data)
+      isLoading(false)
     })
   }, [searchParams])
 
@@ -41,43 +44,44 @@ const HomePage = () => {
   return (
     <>
       <div>
-        Home Page
-        <form onSubmit={updateSearchParams}>
-          <label htmlFor='Title'>Title</label>
-          <input type='text' name='Title' id='Title'></input>
-          <button type='submit'>Search Title</button>
-        </form>
-        <form onSubmit={updateSearchParams}>
-          <label>Author</label>
-          <input type='text' name='Author' id='Author'></input>
-          <button type='submit'>Search Author</button>
-        </form>
-        <form onSubmit={updateSearchParams}>
-          <label>Available By</label>
-          <input type='date' name='Availability' id='Availability'></input>
-          <button type='submit'>Search Available By</button>
-        </form>
-        <form onSubmit={updateSearchParams}>
-          <label>Sort Field</label>
-          <select name='SortBy'>
-            <option value='Title'>Title</option>
-            <option value='Author'>Author</option>
-            <option value='Availability'>Availability</option>
-          </select>
-          <button type='submit'>Sort By Field</button>
-        </form>
-        <form onSubmit={updateSearchParams}>
-          <label>Is Descending</label>
-          <select name='IsDescending' id='IsDescending'>
-            <option value=''></option>
-            <option value='true'>True</option>
-            <option value='false'>False</option>
-          </select>
-          <button type='submit'>Is Descending</button>
-        </form>
-        <button onClick={clearFilters}>
-          Clear Filters
-        </button>
+        <div className='filterContainer'>
+          <form onSubmit={updateSearchParams}>
+            {/* <label htmlFor='Title'>Title</label> */}
+            <input className='input' type='text' name='Title' id='Title'></input>
+            <button type='submit'>Search Title</button>
+          </form>
+          <form onSubmit={updateSearchParams}>
+            {/* <label>Author</label> */}
+            <input className='input' type='text' name='Author' id='Author'></input>
+            <button type='submit'>Search Author</button>
+          </form>
+          <form onSubmit={updateSearchParams}>
+            {/* <label>Available By</label> */}
+            <input className='input' type='date' name='Availability' id='Availability'></input>
+            <button type='submit'>Search Available By</button>
+          </form>
+          <form onSubmit={updateSearchParams}>
+            {/* <label>Sort Field</label> */}
+            <select className='input' name='SortBy'>
+              <option value='Title'>Title</option>
+              <option value='Author'>Author</option>
+              <option value='Availability'>Availability</option>
+            </select>
+            <button type='submit'>Sort By Field</button>
+          </form>
+          <form onSubmit={updateSearchParams}>
+            {/* <label>Is Descending</label> */}
+            <select className='input' name='IsDescending' id='IsDescending'>
+              <option value=''></option>
+              <option value='true'>True</option>
+              <option value='false'>False</option>
+            </select>
+            <button type='submit'>Is Descending</button>
+          </form>
+          <button onClick={clearFilters}>
+            Clear Filters
+          </button>
+        </div>
         {user?.userType === 'Librarian' &&
           <div>
             <button onClick={() => navigate('/add')}>
@@ -86,7 +90,7 @@ const HomePage = () => {
           </div>
         }
         <div className='bookSection'>
-          {books.map((book) => {
+          {loading? 'Loading...' : books.map((book) => {
             return (
               <div className='bookContainer'>
                 <div onClick={() => {navigate(`/book/${book.id}`)}}>
